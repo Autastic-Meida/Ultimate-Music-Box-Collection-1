83656b9ca70a2163456f8d7fcdf863db23a9da02
Gent <82123790+TheGent@users.noreply.github.com>
2025-10-18 16:56:15 +0200

    Update

--- MusicBox/Form_About.vb
+++ MusicBox/Form_About.vb
@@ -4,7 +4,7 @@
 ' - Non-commercial use only
 ' - Attribution to The Autastic Media Dev Team must be preserved
 ' - No warranty is provided; use at your own risk
-' For full details, visit: https://github.com/Autastic-Meida
+' For full details, visit: https://github.com/Autastic-Media
 
 Public Class Form_About
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click, Button2.Click
@@ -12,7 +12,7 @@ Public Class Form_About
     End Sub
 
     Private Sub PictureBox2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox2.Click
-        System.Diagnostics.Process.Start("https://github.com/Autastic-Meida")
+        System.Diagnostics.Process.Start("https://github.com/Autastic-Media")
         Me.Close()
     End Sub
 End Class
